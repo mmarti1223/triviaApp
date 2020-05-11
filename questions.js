a2dffc0bf361b3850d60abc5228c152f0659cd7e
Marcos Marti <53844597+mmarti1223@users.noreply.github.com>
2020-05-11 19:36:06 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+        choices: ["William and Elizabeth", "Joseph and Catherine", "John and Mary", "George and Anne"],
+        answer: "John and Mary"
+    },
+
+    {
+        title: "When did the Liberty Bell get its name?",
+        choices: ["when it was made, in 1701", "when it rang on July 4, 1776", "in the 19th century, when it became a symbol of the abolition of slavery", "none of the above"],
+        answer: "in the 19th century, when it became a symbol of the abolition of slavery"
+    },
+
+    {
+        title: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+        choices: ["home computer", "compact disk player", "cordless phone", "dishwasher"],
+        answer: "compact disk player"
+    },
+
+    {
+        title: "Who holds the record for the most victories in a row on the professional golf tour?",
+        choices: ["Jack Nicklaus", "Arnold Palmer", "Byron Nelson", "Ben Hogan"],
+        answer: "Byron Nelson"
+    },
+
+    {
+        title: "Who is third behind Hank Aaron and Babe Ruth in major league career home runs?",
+        choices: ["Reggie Jackson", "Harmon Killebrew", "Willie Mays", "Frank Robinson"],
+        answer: "Willie Mays"
+    },
+
+    {
+        title: 'What did the "D" in "D-Day" stand for?',
+        choices: ["doom", "day", "Dwight (Eisenhower)", "Dunkirk"],
+        answer: "day"
+    },
+
+
+];
